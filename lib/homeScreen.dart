842bf375ded42e201f0da36201ac1f6a0793a359
VendelaN <njegovec.vendela@gmail.com>
2020-12-14 14:46:59 +0100

    bottom appbar

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'bottomAppBar.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(child: MyAppBar()),
+    );
   }
 }
