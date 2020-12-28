384e9319aaa98eb9cbe5a68d539b5242d43bcc1a
majaulen <maja.ulen@gmail.com>
2020-12-28 17:21:49 +0100

    Implemented advanced filter button

--- lib/Search.dart
+++ lib/Search.dart
@@ -5,7 +5,14 @@ import 'homeScreen.dart';
 
 //Innehåller två klasser atm, Search och Filter. Dela på dem till separata filer?
 
-class Search extends StatelessWidget {
+class Search extends StatefulWidget {
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  var _isPressed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,11 +49,23 @@ class Search extends StatelessWidget {
           children: [
             Container(height: 40),
             _searchInput(),
-            _categoryRow(),
-            _filters(),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 20)),
+                Text('Advanced filter'),
+                IconButton(
+                  icon: Icon(Icons.arrow_drop_down_sharp),
+                  onPressed: () {
+                    setState(() {
+                      _isPressed = !_isPressed;
+                    });
+                  },
+                ), //Kan man flytta ut denna på något smidigt sätt? Den måste ju ligga
+              ],
+            ),
+            if (_isPressed) _filters(),
             Container(height: 10),
             _searchButton(context),
-            //_recipeTitleRow(),
           ],
         ),
       ),
@@ -73,31 +92,29 @@ Widget _searchInput() {
   );
 }
 
-Widget _categoryRow() {
-  return Row(
-    children: [
-      Padding(
-          padding: EdgeInsets.only(left: 20, top: 20),
-          child: Text('Categories',
-              style: TextStyle(fontWeight: FontWeight.bold))),
-    ],
-  );
-}
-
 Widget _filters() {
   return Container(
-    child: Wrap(
-      spacing: 5,
-      runSpacing: 3,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Filter(chipName: 'Meat'),
-        Filter(chipName: 'Chicken'),
-        Filter(chipName: 'Vegetarian'),
-        Filter(chipName: 'Pasta'),
-        Filter(chipName: 'Rice'),
-        Filter(chipName: 'Noodles'),
-        Filter(chipName: 'Pizza'),
-        Filter(chipName: 'Fish'),
+        Padding(
+            padding: EdgeInsets.only(left: 0, top: 20, bottom: 10),
+            child: Text('Categories',
+                style: TextStyle(fontWeight: FontWeight.bold))),
+        Wrap(
+          spacing: 5,
+          runSpacing: 3,
+          children: [
+            Filter(chipName: 'Meat'),
+            Filter(chipName: 'Chicken'),
+            Filter(chipName: 'Vegetarian'),
+            Filter(chipName: 'Pasta'),
+            Filter(chipName: 'Rice'),
+            Filter(chipName: 'Noodles'),
+            Filter(chipName: 'Pizza'),
+            Filter(chipName: 'Fish'),
+          ],
+        ),
       ],
     ),
   );
@@ -105,28 +122,18 @@ Widget _filters() {
 
 Widget _searchButton(BuildContext context) {
   return Container(
-    width: 350,
+    width: 380,
     child: RaisedButton(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
       onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => SearchResult()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SearchResult()));
       },
       color: Color(0xFF6C804B),
       child: Text('Search', style: TextStyle(color: Colors.white)),
     ),
   );
 }
-
-/*Widget _recipeTitleRow() {
-  return Row(
-    children: [
-      Padding(
-          padding: EdgeInsets.only(left: 20, top: 20),
-          child: Text('357 recipes found',
-              style: TextStyle(fontWeight: FontWeight.bold))),
-    ],
-  );
-}*/
 
 class Filter extends StatefulWidget {
   final String chipName;
